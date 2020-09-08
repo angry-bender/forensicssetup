07b2f31ac9a7bbb8d052ebcf45d7fceb6c290ea9
angry-bender <samfree91@hotmail.com>
2020-09-08 20:02:19 +0930

    Fixed Commenting, Improved Saltstack integration

--- Get-Forensics-Tools.ps1
+++ Get-Forensics-Tools.ps1
@@ -1,23 +1,23 @@
 function Get-ZippedDownload([String] $name, [String] $dl_url)
 {
-        $zip = "$name.zip"
+    #Download Zip    
+    $zip = "$name.zip"
     
-        Write-Host Dowloading $name
-        Invoke-WebRequest $dl_url -OutFile $zip
-    
-        Write-Host Extracting release files
-        Expand-Archive $zip -Force
+    Write-Host Dowloading $name
+    Invoke-WebRequest $dl_url -OutFile $zip
+
+    #Extract Zip    
+    Write-Host Extracting release files
+    Expand-Archive $zip -Force
        
-        # Removing temp files
-        Remove-Item $zip -Force
+    # Removing temp files
+    Remove-Item $zip -Force
 }
 function Get-CyLR
 {
-    # Download latest release from github
+    # Check Latest Windows x64 release
     $owner = "orlikoski"
     $repo = "CyLR"
-    $file = "$repo.zip"
-
     $releases = "https://api.github.com/repos/$owner/$repo/releases"
 
     Write-Host Determining latest release of $repo
@@ -25,6 +25,7 @@ function Get-CyLR
     $name = (Invoke-WebRequest $releases | ConvertFrom-Json)[0].name
     $zip = "$name.zip"
 
+    #Download Latest Release
     Write-Host Dowloading latest release of $name
     Invoke-WebRequest $download -Out $zip
 
@@ -36,28 +37,37 @@ function Get-CyLR
 }
 function Get-Dcode()
 {
+    #check for the latest version
     $webresponse =  Invoke-WebRequest "https://www.digital-detective.net/dcode" -useBasicParsing 
-    $dl_url = $webresponse.Links | where href -Like "*download*" | where href -notlike "*downloads*"
+    $dl_url = $webresponse.Links | Where-Object href -Like "*download*" | Where-Object href -notlike "*downloads*"
 
     Get-ZippedDownload "Dcode" $dl_url.href
 }
 
 function Get-SansPosters()
 {
-    $DesktopPath = [Environment]::GetFolderPath("CommonDesktopDirectory")
+    # $DesktopPath = [Environment]::GetFolderPath("CommonDesktopDirectory")
 
-    $raw_url = "https://raw.githubusercontent.com/teamdfir/sift-saltstack/master/sift/files/sift/resources/"
-    Start-BitsTransfer -source $raw_url"Evidence-of-Poster.pdf"
-    Start-BitsTransfer -source $raw_url"Find-Evil-Poster.pdf"
-    Start-BitsTransfer -source $raw_url"SANS-DFIR.pdf"
-    Start-BitsTransfer -source $raw_url"Smartphone-Forensics-Poster.pdf"
-    Start-BitsTransfer -source $raw_url"memory-forensics-cheatsheet.pdf"
-    Start-BitsTransfer -source $raw_url"network-forensics-cheatsheet.pdf"
-    Start-BitsTransfer -source $raw_url"sift-cheatsheet.pdf"
-    Start-BitsTransfer -source $raw_url"windows-to-unix-cheatsheet.pdf"
+    $owner = "teamdfir"
+    $repo = "sift-saltstack"
+    $apiurl = "https://api.github.com/repos"
 
-    New-Item -Path $DesktopPath -Name "SANS_Posters" -ItemType "directory"
+    #Master Branch's sha
+    $masterbranch = (Invoke-WebRequest "$apiurl/$owner/$repo/branches/master"| ConvertFrom-Json)
+    $sifttree = (Invoke-WebRequest $masterbranch.commit.commit.tree.url | ConvertFrom-Json).tree | Where-Object path -eq sift
+    $filestree = (Invoke-WebRequest $sifttree.url | ConvertFrom-Json).tree | Where-Object path -eq files
+    $sift2tree = (Invoke-WebRequest $filestree.url | ConvertFrom-Json).tree | Where-Object path -eq sift
+    $resourcestree = (Invoke-WebRequest $sift2tree.url | ConvertFrom-Json).tree | Where-Object path -eq resources
     
+    #Gets the filelisting from the master tree
+    $files = (Invoke-WebRequest $resourcestree.url| ConvertFrom-Json)[0].tree
+
+    #Downloads the files
+    foreach ($file in $files)
+    {
+        $filename = $file.path
+        Start-BitsTransfer -source "https://raw.githubusercontent.com/$owner/$repo/master/sift/files/sift/resources/$filename"
+    }
 }
 
 function Install-FTK
@@ -86,5 +96,6 @@ Set-Location C:\NonChoco_Tools
 # Start Getting and Installing other Executables
 Get-CyLR
 Get-Dcode
+Get-SansPosters
 
 
--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # forensicssetup
 
 ## Pre-requisites
-Windows 10 1904 or above
+64 Bit Windows 10 1904 or above
 Internet Explorer opened, with default settings selected
+
+
+## Usage
+Install Choclatey with 
+Get-Choclatey.ps1
+
+Install Git with 
+Get-Git.ps1
+
+Install Forensics Tools with
+Get-Forensics-Tools.ps1
