24307c6b792200e185997643744e9df24242185a
angry-bender <samfree91@hotmail.com>
2020-09-08 21:02:18 +0930

    Beta V0.1

--- Get-Forensics-Tools.ps1
+++ Get-Forensics-Tools.ps1
@@ -1,7 +1,7 @@
 function Get-ZippedDownload([String] $name, [String] $dl_url)
 {
     #Download Zip    
-    $zip = "$name.zip"
+    $zip = "$($name).zip"
     
     Write-Host Dowloading $name
     Invoke-WebRequest $dl_url -OutFile $zip
@@ -18,7 +18,7 @@ function Get-CyLR
     # Check Latest Windows x64 release
     $owner = "orlikoski"
     $repo = "CyLR"
-    $releases = "https://api.github.com/repos/$owner/$repo/releases"
+    $releases = "https://api.github.com/repos/$($owner)/$($repo)/releases"
 
     Write-Host Determining latest release of $repo
     $download = (Invoke-WebRequest $releases | ConvertFrom-Json)[0].assets[2].browser_download_url
@@ -44,40 +44,63 @@ function Get-Dcode()
     Get-ZippedDownload "Dcode" $dl_url.href
 }
 
-function Get-SansPosters()
+function Get-SansResources()
 {
-    # $DesktopPath = [Environment]::GetFolderPath("CommonDesktopDirectory")
+    $DesktopPath = [Environment]::GetFolderPath("CommonDesktopDirectory")
 
     $owner = "teamdfir"
     $repo = "sift-saltstack"
     $apiurl = "https://api.github.com/repos"
 
     # Creates an object, for each of the relevant sans sift-saltstack trees
-    $masterbranch = (Invoke-WebRequest "$apiurl/$owner/$repo/branches/master"| ConvertFrom-Json)
+    $masterbranch = (Invoke-WebRequest "$($apiurl)/$($owner)/$($repo)/branches/master"| ConvertFrom-Json)
     $sifttree = (Invoke-WebRequest $masterbranch.commit.commit.tree.url | ConvertFrom-Json).tree | Where-Object path -eq sift
     $filestree = (Invoke-WebRequest $sifttree.url | ConvertFrom-Json).tree | Where-Object path -eq files
     $sift2tree = (Invoke-WebRequest $filestree.url | ConvertFrom-Json).tree | Where-Object path -eq sift
     $resourcestree = (Invoke-WebRequest $sift2tree.url | ConvertFrom-Json).tree | Where-Object path -eq resources
+    $imagestree = (Invoke-WebRequest $sift2tree.url | ConvertFrom-Json).tree | Where-Object path -eq images
     
     #Creates a tree oject from /sift/files/sift/resources where posters are located
     $posters = (Invoke-WebRequest $resourcestree.url| ConvertFrom-Json)[0].tree
+    $images =  (Invoke-WebRequest $imagestree.url| ConvertFrom-Json)[0].tree
+
+    #Creates the working directory
+    New-Item -Path $DesktopPath -Name "SANS_Posters" -ItemType "directory"
+    
 
     #Downloads the posters
     foreach ($poster in $posters)
     {
         $filename = $poster.path
-        Start-BitsTransfer -source "https://raw.githubusercontent.com/$owner/$repo/master/sift/files/sift/resources/$filename"
+        Start-BitsTransfer -source "https://raw.githubusercontent.com/$($owner)/$($repo)/master/sift/files/sift/resources/$($filename)"
+        Move-Item $filename "$($DesktopPath)\SANS_Posters"
     }
+    
+    #Downloads the images
+    foreach ($image in $images)
+    {
+        $filename = $image.path
+        Start-BitsTransfer -source "https://raw.githubusercontent.com/$($owner)/$($repo)/master/sift/files/sift/images/$($filename)"
+        Move-Item $filename "$($DesktopPath)\"
+    }
+
+    
 }
 
-function Install-FTK
+function Get-FTK
 {
-    Start-BitsTransfer -Source "https://ad-iso.s3.amazonaws.com/AD_Forensic_Tools_7.3.0.iso"
-    Mount-DiskImage AD_Forensic_Tools_7.3.0.iso
+    # Start-BitsTransfer -Source "https://ad-iso.s3.amazonaws.com/AD_Forensic_Tools_7.3.0.iso"
+    Write-Host "Installing FTK, This is only available by GUI Installer, and cannot be done unattended"
+    Mount-DiskImage $pwd\AD_Forensic_Tools_7.3.0.iso | Out-Null
+    $driveletter = (Get-DiskImage C:\test\AD_Forensic_Tools_7.3.0.iso | Get-Volume).DriveLetter
+    Write-host "The script is paused until installation is complete, including the installation cleanup (might take upto 3 minutes after clicking finish)"
+    Start-Process "$($driveletter):\Imager\AccessData_FTK_Imager_(x64).exe" -ArgumentList "/S /V /qn" -wait    
+    Dismount-DiskImage $pwd\AD_Forensic_Tools_7.3.0.iso | Out-Null
 }
 
 Import-Module BitsTransfer
 
+# Get the choco packages installed
 choco install sysinternals
 choco install nirlauncher
 choco install photorec
@@ -87,13 +110,15 @@ choco install sleuthkit
 choco install network-miner
 choco install exiftoolgui
 
-# Create a working directory for other executables
+# Create a working directory for other executables then work there
 New-Item -Path "C:\" -Name "NonChoco_Tools" -ItemType "directory"
 Set-Location C:\NonChoco_Tools
 
 # Start Getting and Installing other Executables
 Get-CyLR
 Get-Dcode
-Get-SansPosters
+Get-SansResources
+Get-FTK
+
 
 
