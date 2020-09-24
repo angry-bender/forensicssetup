1572c7363d19725bb8fb60fc1109bc7236c1e3b6
angry-bender <samfree91@hotmail.com>
2020-09-24 22:36:28 +0930

    Fixed Popup window

--- Get-Forensics-Tools.ps1
+++ Get-Forensics-Tools.ps1
@@ -236,7 +236,12 @@ $WShell = New-Object -com "Wscript.Shell"
 & powercfg -change -hibernate-timeout-dc 0 | Out-Null
 
 #Open internet exporer
-[System.Windows.MessageBox]::Show('Please setup Internet Explorer 11, By selecting "Use reccommended security settings" and closing')
+Add-Type -AssemblyName PresentationCore,PresentationFramework
+$ButtonType = [System.Windows.MessageBoxButton]::Ok
+$MessageboxTitle = "Setup Requirements"
+$Messageboxbody = 'Please setup Internet Explorer 11, By selecting "Use reccommended security settings" and closing'
+$MessageIcon = [System.Windows.MessageBoxImage]::Warning
+[System.Windows.MessageBox]::Show($Messageboxbody,$MessageboxTitle,$ButtonType,$messageicon)
 start-process iexplore.exe -Wait
 
 #installs packages that require refreshenv
